d20d44f7e23e482828e84d795f22f38980a7200b
Ayotheman12 <150096498+Ayotheman12@users.noreply.github.com>
2024-04-23 20:10:49 -0400

    Add files via upload

---
+++ models/Comment.js
@@ -0,0 +1,53 @@
+// id, content, date created, post id, user id
+// Import statements
+const {Model, DataTypes} = require("sequelize");
+const sequelize = require("../config/connection");
+
+// Create new Model object
+class Comment extends Model {}
+
+// Initialize the Comment model
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "user",
+                key: "id"
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "post",
+                key: "id"
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "comment"
+    }
+);
+
+module.exports = Comment;
